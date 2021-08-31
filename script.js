a1fc454d385485d7480eb4d7c07a33e56b9149ec
chiemerie-arum <emerie.arum@yahoo.com>
2021-09-01 01:13:17 +0200

    implement smooth scrolling

--- script.js
+++ script.js
@@ -29,3 +29,28 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+const buttonScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+buttonScrollTo.addEventListener('click', e => {
+  const s1coords = section1.getBoundingClientRect();
+  // console.log(s1coords);
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+////////////////////////////////////////////////
+//Page Navigation
+//Event delegation steps
+//1. add the event listener to the common element
+//2. determine what element originated the event
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  //matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    e.preventDefault();
+    const id = e.target.getAttribute('href');
+    console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
